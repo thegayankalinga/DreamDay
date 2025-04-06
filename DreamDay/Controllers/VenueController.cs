475ff849ff01d508b89123471f46bcd06dda786c
Bulathsinhalage Gayan Kalinga <bg15407@gmail.com>
2025-04-06 22:58:04 +0530

    Some Admin work to dashboard

--- DreamDay/Controllers/VenueController.cs
+++ DreamDay/Controllers/VenueController.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DreamDay.Data;
+using DreamDay.Interfaces;
+using DreamDay.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DreamDay.Controllers;
 
 public class VenueController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly IVenueRepository _venueRepository;
+
+    public VenueController(IVenueRepository venueRepository)
     {
-        return View();
+        _venueRepository = venueRepository;
     }
+    
+    // GET: Venue (Public view)
+    public async Task<IActionResult> Index()
+    {
+        var venues = await _venueRepository.GetAllVenuesAsync();
+        return View(venues);
+    }
+    
+    // GET: Venue/Details/5
+    public async Task<IActionResult> Details(int id)
+    {
+        var venue = await _venueRepository.GetByIdAsync(id);
+        if (venue == null)
+        {
+            return NotFound();
+        }
+        return View(venue);
+    }
+
+    #region Admin Functions
+    
+    // GET: Admin/Venues
+    [Authorize(Roles = UserRoles.Admin)]
+    [HttpGet("Admin/Venues")]
+    public async Task<IActionResult> AdminVenues()
+    {
+        var venues = await _venueRepository.GetAllVenuesAsync();
+        return View("~/Views/Admin/Venues.cshtml", venues);
+    }
+    
+    // POST: Venue/Create
+    [Authorize(Roles = UserRoles.Admin)]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Venue venue)
+    {
+        if (ModelState.IsValid)
+        {
+            await _venueRepository.AddAsync(venue);
+            return RedirectToAction("AdminVenues", "Venue");
+        }
+        return RedirectToAction("AdminVenues", "Venue");
+    }
+
+    // POST: Venue/Edit/5
+    [Authorize(Roles = UserRoles.Admin)]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Venue venue)
+    {
+        if (id != venue.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            await _venueRepository.UpdateAsync(venue, id);
+            return RedirectToAction("AdminVenues", "Venue");
+        }
+        return RedirectToAction("AdminVenues", "Venue");
+    }
+
+    // POST: Venue/Delete/5
+    [Authorize(Roles = UserRoles.Admin)]
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _venueRepository.DeleteAsync(id);
+        return RedirectToAction("AdminVenues", "Venue");
+    }
+    
+    #endregion
 }
